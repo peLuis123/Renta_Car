440f61120eabd303b4632b2111dd52e4d98dde06
peLuis123 <pedrorc2018@gmail.com>
2024-02-12 10:39:36 -0500

    first commit

---
+++ src/layouts/default/DefaultLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- en caso que quieras crear un sidebard y un app-bar se agrega aqui y ya se aplican a todas las rutas hijas -->
+  <v-app>
+    <default-view />
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+  import DefaultView from './View.vue'
+  // const hora =  Date.time
+  const count = ref(0)
+  console.log(count)
+  // console.log(hora)
+  // function hora(){
+
+  // }
+</script>
