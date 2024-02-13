1297b61098c0a399c3f8133a478c91cfa4b97176
peLuis123 <pedrorc2018@gmail.com>
2024-02-13 16:21:31 -0500

    feat: repeatable components

---
+++ src/components/CheckList.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-icon
+    size="32px"
+    color="orange"
+    class="mr-2"
+  >
+    mdi-check-circle
+  </v-icon> {{ item.text }}
+</template>
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+  item:{type: Object, default: () =>{}}
+})
+</script>
---
+++ src/components/OffertsCards.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card
+    class="ma-2 pa-2 d-flex align-center"
+    style="width: auto; min-width: 120px; "
+  >
+    <div
+      class="pa-5"
+    >
+      <v-icon
+        size="35"
+        color="orange"
+      >
+        mdi-tag
+      </v-icon>
+    </div>
+    <div
+      class="text-center"
+      style="width: 80%;"
+    >
+      <h1>{{ offert.discount }}</h1>
+      <div>{{ offert.infoExtra }}</div>
+    </div>
+  </v-card>
+</template>
+<script setup>
+defineProps({
+    offert:{type: Object, default: () =>{}}
+  })
+</script>
