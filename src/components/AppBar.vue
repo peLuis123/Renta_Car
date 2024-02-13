911d8d8f69381a1f149235b030f7b5b809800536
peLuis123 <pedrorc2018@gmail.com>
2024-02-13 09:58:34 -0500

    feat: add appbar

---
+++ src/components/AppBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app-bar
+    :elevation="2"
+  >
+    <v-app-bar-title>Renta Car</v-app-bar-title>
+    <div
+      v-for="(item) in items"
+      :key="item.id"
+    >
+      <v-btn
+        variant="plain"
+        :to="item.to"
+      >
+        {{ item.text }}
+      </v-btn>
+    </div>
+
+    <v-icon class="ma-5">
+      mdi-cart
+    </v-icon>
+  </v-app-bar>
+</template>
+<script setup>
+import { ref } from 'vue'
+const items = ref([
+    { id: 1, text: 'home', to: "/home" },
+    { id: 2, text: 'listings', to: "/listings" },
+    { id: 3, text: 'contact', to: "/listings" },
+    { id: 4, text: 'landing', to: "/listings" },
+    { id: 5, text: 'services', to: "/listings" },
+    { id: 6, text: 'about', to: "/listings" },
+    { id: 7, text: 'listing', to: "/listings" }
+]);
+</script>
