a795d06cba0503a12af15a91612b4d706a39da46
peLuis123 <pedrorc2018@gmail.com>
2024-02-13 16:22:52 -0500

    feat: home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,16 @@
   <HomePortada />
   <HomeReserves />
   <HomeVehicles />
+  <HomeOfferts />
+  <HomeRates />
+  <ContactMe />
 </template>
 
-  <script setup>
-  import HomePortada from "@/components/HomePortada.vue"
-  import HomeReserves from "@/components/HomeReserves.vue"
-  import HomeVehicles from "@/components/HomeVehicles.vue"
-  </script>
+<script setup>
+import HomePortada from "@/components/HomePortada.vue"
+import HomeReserves from "@/components/HomeReserves.vue"
+import HomeVehicles from "@/components/HomeVehicles.vue"
+import HomeOfferts from "@/components/HomeOfferts.vue";
+import HomeRates from "@/components/HomeRates.vue";
+import ContactMe from "@/components/ContactMe.vue"
+</script>
