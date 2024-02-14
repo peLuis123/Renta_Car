15f83e28747079cf82371192f278a35a61b58c45
peLuis123 <pedrorc2018@gmail.com>
2024-02-13 19:04:32 -0500

    feat: the repeatable elements for contact were componentized

---
+++ src/components/ContactInfo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    v-for="(item) in items"
+    :key="item.value"
+    class="ma-7"
+  >
+    <v-icon class="mr-3">
+      {{ item.icon }}
+    </v-icon> {{ item.title }}
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+const items = ref([
+    {
+        title: 'pedrorc2018@gmail.com',
+        icon: 'mdi-email',
+        value: 1,
+    },
+    {
+        title: '+51 958 104 634',
+        value: 2,
+        icon: 'mdi-phone-message',
+    },
+    {
+        title: 'calle random 123 francia',
+        value: 3,
+        icon: 'mdi-map-marker',
+    },
+],)
+</script>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,11 @@
+<template>
+  <ContactPortada />
+  <ContactAsked />
+  <ContactReserve />
+</template>
+
+<script setup>
+import ContactPortada from '@/components/ContactPortada.vue';
+import ContactAsked from '@/components/ContactAsked.vue';
+import ContactReserve from '@/components/ContactReserve.vue';
+</script>
